a821aaeef96b3dda361181f0907ee4896d7c598c
Tingyi <tingyity@amazon.com>
2022-10-05 15:48:41 +0200

    add data model

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
       </div>
       <table border="1">
         <thead>
-          <tr><td>Id</td><td>Desc</td></tr>
+          <tr><td>Id</td><td>Description</td></tr>
         </thead>
         <tbody>
           {todos.map( (item,i) => {
